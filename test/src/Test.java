f074018feb6018a1c66fcde8fe72e099e71aa168
caoss <1625466237@qq.com>
2026-05-26 21:10:16 +0800

    Merge branch 'master' of https://github.com/caoshuai123/tt into HEAD
    # Conflicts:
    #	test/src/Test.java

--- test/src/Test.java
+++ test/src/Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("你是笨蛋");
+        System.out.println("你是笨蛋1号");
         System.out.println("你是笨蛋2号");
         System.out.println("你是笨蛋3号");
         System.out.println("你是笨蛋4号");
