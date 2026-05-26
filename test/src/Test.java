f9ecb32ea72a62e7c126de7974230d4e1eaf3db1
caoshuai123 <52746576+caoshuai123@users.noreply.github.com>
2026-05-26 20:16:52 +0800

    Update Test.java

--- test/src/Test.java
+++ test/src/Test.java
@@ -2,6 +2,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("你是笨蛋");
+        System.out.println("你是笨蛋2号");
         System.out.println("你是笨蛋3号");
     }
 }
